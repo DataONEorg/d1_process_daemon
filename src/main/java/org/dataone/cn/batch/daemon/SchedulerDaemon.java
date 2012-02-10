6fa74fd496ad2114d2e0becbff74788074315cda
Robert Waltz <rwaltz@utk.edu>
2012-02-10 07:53:15 +0000

    Preparing for RC1 release #2093 Implement Log Aggregation

--- src/main/java/org/dataone/cn/batch/daemon/SchedulerDaemon.java
+++ src/main/java/org/dataone/cn/batch/daemon/SchedulerDaemon.java
@@ -20,8 +20,16 @@ package org.dataone.cn.batch.daemon;
 /* @version $Id: ServiceDaemon.java 897078 2010-01-08 01:52:47Z sebb $ */
 import com.hazelcast.core.Hazelcast;
 import java.util.Collection;
+import java.util.Iterator;
 import org.apache.commons.daemon.Daemon;
 import org.apache.commons.daemon.DaemonContext;
+import org.dataone.cn.ldap.NodeAccess;
+import org.dataone.cn.ldap.ProcessingState;
+import org.dataone.configuration.Settings;
+
+
+import org.dataone.service.exceptions.ServiceFailure;
+import org.dataone.service.types.v1.NodeReference;
 
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
@@ -42,7 +50,7 @@ public class SchedulerDaemon implements Daemon {
 
     private String appContextLocation = "file:/etc/dataone/batch/applicationContext.xml";
     private ApplicationContext context;
-
+    static final String localCnIdentifier = Settings.getConfiguration().getString("cn.nodeId");
     public SchedulerDaemon() {
         super();
         System.out.println("ServiceDaemon: instance " + this.hashCode()
@@ -83,19 +91,52 @@ public class SchedulerDaemon implements Daemon {
         /* Dump a message */
         System.out.println("ServiceDaemon: stopping");
         try {
+
             SchedulerFactory schedulerFactory = new StdSchedulerFactory();
             Collection<Scheduler> quartzSchedulers = schedulerFactory.getAllSchedulers();
-            for (Scheduler scheduler : quartzSchedulers) {
-                scheduler.shutdown(true);
-                System.out.println("Quartz scheduler " + scheduler.getSchedulerName() + " stopped");
+            int numOfSchedulers = quartzSchedulers.size();
+            System.out.println("Found " + numOfSchedulers + " Quartz schedulers");
+            Scheduler[] schedulers = new Scheduler[numOfSchedulers];
+            schedulers = quartzSchedulers.toArray(schedulers);
+
+
+            for (int i = 0; i < numOfSchedulers; i++){
+                if (!schedulers[i].isShutdown()) {
+                    System.out.println("Stopping Quartz scheduler " + schedulers[i].getSchedulerName());
+                    schedulers[i].shutdown(true);
+                }
             }
 
-            System.out.println("ServiceDaemon: stopped");
-        } catch (SchedulerException ex) {
-            System.err.println("Shutting down Quartz scheduler failed: " + ex.getMessage());
+            /* the code below blows up with
+             * java.util.ConcurrentModificationException
+             * at java.util.HashMap$HashIterator.nextEntry(HashMap.java:793)
+	     * at java.util.HashMap$ValueIterator.next(HashMap.java:822)
+	     * at java.util.Collections$UnmodifiableCollection$1.next(Collections.java:1010)
+
+                for  (Scheduler scheduler : quartzSchedulers) {
+                    System.out.println("Stopping Quartz scheduler " + scheduler.getSchedulerName());
+                    if (!scheduler.isShutdown()) {
+                        scheduler.shutdown(true);
+                    }
+                    System.out.println("Quartz scheduler " + scheduler.getSchedulerName() + " stopped");
+                }
+             */
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.err.println("Shutting down Quartz scheduler failed: ");
         }
         Hazelcast.shutdownAll();
-        
+        NodeAccess nodeAccess = new NodeAccess();
+        NodeReference localNodeReference = new NodeReference();
+        localNodeReference.setValue(localCnIdentifier);
+        try {
+            nodeAccess.setProcessingState(localNodeReference, ProcessingState.Offline);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            System.err.println("Updating Process State failed");
+        }
+        System.out.println("ServiceDaemon: stopped");
     }
 
     @Override
