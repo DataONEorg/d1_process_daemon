4e5ce2cc789c2eaa807b93953c8213833b76a649
Andy Pippin <pippin@nceas.ucsb.edu>
2012-04-23 23:06:58 +0000

    Adding changes for copyright.

--- src/main/java/org/dataone/cn/batch/daemon/SchedulerDaemon.java
+++ src/main/java/org/dataone/cn/batch/daemon/SchedulerDaemon.java
@@ -1,23 +1,27 @@
-package org.dataone.cn.batch.daemon;
-
-/*
- *  Licensed to the Apache Software Foundation (ASF) under one or more
- *  contributor license agreements.  See the NOTICE file distributed with
- *  this work for additional information regarding copyright ownership.
- *  The ASF licenses this file to You under the Apache License, Version 2.0
- *  (the "License"); you may not use this file except in compliance with
- *  the License.  You may obtain a copy of the License at
+/**
+ * This work was created by participants in the DataONE project, and is
+ * jointly copyrighted by participating institutions in DataONE. For 
+ * more information on DataONE, see our web site at http://dataone.org.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *   Copyright ${year}
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ * 
+ * $Id: ServiceDaemon.java 897078 2010-01-08 01:52:47Z sebb $
  */
 
-/* @version $Id: ServiceDaemon.java 897078 2010-01-08 01:52:47Z sebb $ */
+package org.dataone.cn.batch.daemon;
+
 import com.hazelcast.core.Hazelcast;
 import java.util.Collection;
 import org.apache.commons.daemon.Daemon;
