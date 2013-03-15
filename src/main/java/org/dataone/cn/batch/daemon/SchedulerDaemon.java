309352af5222445bd91fa0be49b3248fb8b2cc94
Sky Roseboom <sroseboo@epscor.unm.edu>
2013-03-15 21:32:49 +0000

    replication factory introduced to provide common service objects to replication components.

--- src/main/java/org/dataone/cn/batch/daemon/SchedulerDaemon.java
+++ src/main/java/org/dataone/cn/batch/daemon/SchedulerDaemon.java
@@ -22,29 +22,28 @@
 
 package org.dataone.cn.batch.daemon;
 
-import com.hazelcast.core.Hazelcast;
 import java.util.Collection;
+
 import org.apache.commons.daemon.Daemon;
 import org.apache.commons.daemon.DaemonContext;
 import org.dataone.cn.ldap.NodeAccess;
 import org.dataone.cn.ldap.ProcessingState;
 import org.dataone.configuration.Settings;
-
-
+import org.dataone.service.cn.replication.v1.audit.ScheduledReplicationAuditController;
 import org.dataone.service.types.v1.NodeReference;
-
 import org.quartz.Scheduler;
 import org.quartz.SchedulerFactory;
 import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
+import com.hazelcast.core.Hazelcast;
+
 /**
- * Implements Apache Commons Daemon interface.
- * The Class will start up a Spring Application context configured in a set location
- * The Class assumes that it will be running a Quartz scheduler  that will be shutdown
- * when the process ends.
+ * Implements Apache Commons Daemon interface. The Class will start up a Spring
+ * Application context configured in a set location The Class assumes that it
+ * will be running a Quartz scheduler that will be shutdown when the process
+ * ends.
  * 
  * @author rwaltz
  */
@@ -53,28 +52,28 @@ public class SchedulerDaemon implements Daemon {
     private String appContextLocation = "file:/etc/dataone/process/applicationContext.xml";
     private ApplicationContext context;
     static final String localCnIdentifier = Settings.getConfiguration().getString("cn.nodeId");
+
+    private static final String SCHEDULED_REPLICA_AUDIT_NAME = "scheduledReplicationAuditController";
+    private ScheduledReplicationAuditController replicationAuditScheduler;
+
     public SchedulerDaemon() {
         super();
-        System.out.println("ServiceDaemon: instance " + this.hashCode()
-                + " created");
+        System.out.println("ServiceDaemon: instance " + this.hashCode() + " created");
     }
 
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
-        System.out.println("ServiceDaemon: instance " + this.hashCode()
-                + " garbage collected");
+        System.out.println("ServiceDaemon: instance " + this.hashCode() + " garbage collected");
     }
 
     /**
      * init and destroy were added in jakarta-tomcat-daemon.
      */
     @Override
-    public void init(DaemonContext context)
-            throws Exception {
+    public void init(DaemonContext context) throws Exception {
         /* Set the err */
-        System.out.println("ServiceDaemon: instance " + this.hashCode()
-                + " init");
+        System.out.println("ServiceDaemon: instance " + this.hashCode() + " init");
 
         System.out.println("ServiceDaemon: init done ");
 
@@ -83,10 +82,14 @@ public class SchedulerDaemon implements Daemon {
     @Override
     public void start() {
         /* Dump a message */
-          System.out.println("ServiceDaemon: starting");
-          context = new FileSystemXmlApplicationContext(appContextLocation);
-//          context = new ClassPathXmlApplicationContext(new String[]{"/org/dataone/configuration/applicationContext.xml"});
-          System.out.println("ServiceDaemon: started");
+        System.out.println("ServiceDaemon: starting");
+        context = new FileSystemXmlApplicationContext(appContextLocation);
+        // context = new ClassPathXmlApplicationContext(new
+        // String[]{"/org/dataone/configuration/applicationContext.xml"});
+
+        startScheduledReplicationAuditing();
+
+        System.out.println("ServiceDaemon: started");
     }
 
     @Override
@@ -102,29 +105,30 @@ public class SchedulerDaemon implements Daemon {
             Scheduler[] schedulers = new Scheduler[numOfSchedulers];
             schedulers = quartzSchedulers.toArray(schedulers);
 
-
-            for (int i = 0; i < numOfSchedulers; i++){
+            for (int i = 0; i < numOfSchedulers; i++) {
                 if (!schedulers[i].isShutdown()) {
-                    System.out.println("Stopping Quartz scheduler " + schedulers[i].getSchedulerName());
+                    System.out.println("Stopping Quartz scheduler "
+                            + schedulers[i].getSchedulerName());
                     schedulers[i].shutdown(true);
                 }
             }
 
-            /* the code below blows up with
-             * java.util.ConcurrentModificationException
-             * at java.util.HashMap$HashIterator.nextEntry(HashMap.java:793)
-	     * at java.util.HashMap$ValueIterator.next(HashMap.java:822)
-	     * at java.util.Collections$UnmodifiableCollection$1.next(Collections.java:1010)
-
-                for  (Scheduler scheduler : quartzSchedulers) {
-                    System.out.println("Stopping Quartz scheduler " + scheduler.getSchedulerName());
-                    if (!scheduler.isShutdown()) {
-                        scheduler.shutdown(true);
-                    }
-                    System.out.println("Quartz scheduler " + scheduler.getSchedulerName() + " stopped");
-                }
+            /*
+             * the code below blows up with
+             * java.util.ConcurrentModificationException at
+             * java.util.HashMap$HashIterator.nextEntry(HashMap.java:793) at
+             * java.util.HashMap$ValueIterator.next(HashMap.java:822) at
+             * java.util
+             * .Collections$UnmodifiableCollection$1.next(Collections.java:1010)
+             * 
+             * for (Scheduler scheduler : quartzSchedulers) {
+             * System.out.println("Stopping Quartz scheduler " +
+             * scheduler.getSchedulerName()); if (!scheduler.isShutdown()) {
+             * scheduler.shutdown(true); }
+             * System.out.println("Quartz scheduler " +
+             * scheduler.getSchedulerName() + " stopped"); }
              */
-            
+
         } catch (Exception ex) {
             ex.printStackTrace();
             System.err.println("Shutting down Quartz scheduler failed: ");
@@ -139,12 +143,32 @@ public class SchedulerDaemon implements Daemon {
             ex.printStackTrace();
             System.err.println("Updating Process State failed");
         }
+
+        stopScheduledReplicationAuditing();
+
         System.out.println("ServiceDaemon: stopped");
+    }
+
+    private void stopScheduledReplicationAuditing() {
+        if (replicationAuditScheduler != null) {
+            System.out.println("Stopping replication audit scheduler....");
+            replicationAuditScheduler.shutdown();
+        }
+    }
+
+    private void startScheduledReplicationAuditing() {
+        try {
+            replicationAuditScheduler = (ScheduledReplicationAuditController) context
+                    .getBean(SCHEDULED_REPLICA_AUDIT_NAME);
+            replicationAuditScheduler.startup();
+        } catch (Exception e) {
+            System.out.println("ServiceDaemon: unable to start Replication Audit Scheduler.");
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void destroy() {
-        System.out.println("ServiceDaemon: instance " + this.hashCode()
-                + " destroy");
+        System.out.println("ServiceDaemon: instance " + this.hashCode() + " destroy");
     }
 }
